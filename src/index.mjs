d1e7f464288e60970274861d63a0d73159c2a7bf
ShahdadKh <mr.khoshbakht91@gmail.com>
2024-07-15 23:37:07 +0330

    get request

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,52 @@
 import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const mochUsers = [
+  { id: 1, username: "gfawltey0", displayname: "sclemmow0" },
+  { id: 2, username: "bbrophy1", displayname: "jbernardez1" },
+  { id: 3, username: "ikillingsworth2", displayname: "bhaddinton2" },
+  { id: 4, username: "rdorning3", displayname: "cgosnold3" },
+  { id: 5, username: "jgiovannelli4", displayname: "kbaynam4" },
+  { id: 6, username: "glankester5", displayname: "rlambin5" },
+  { id: 7, username: "edawidowicz6", displayname: "plineen6" },
+  { id: 8, username: "gwaber7", displayname: "bdamant7" },
+  { id: 9, username: "thulbert8", displayname: "lcasassa8" },
+  { id: 10, username: "hbleakman9", displayname: "relster9" },
+];
+
+app.get("/", (request, response) => {
+  response.send({ msg: "hello world" });
+});
+
+// app.get("/api/users", (request, response) => {
+//   return response.send(mochUsers);
+// });
+
+//localhost/api/users?filter=username&value=gfa
+app.get("/api/users", (request, response) => {
+  console.log(request.query);
+  const {
+    query: { filter, value },
+  } = request;
+  if (filter && value)
+    return response.send(
+      mochUsers.filter((user) => user[filter].includes(value))
+    );
+  return response.send(mochUsers);
+});
+
+app.get("/api/users/:id", (request, response) => {
+  const parsedId = parseInt(request.params.id);
+  if (isNaN(parsedId))
+    return response.status(400).send({ msg: "bad request. invalid Id." });
+  const findUser = mochUsers.find((user) => user.id === parsedId);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+});
