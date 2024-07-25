56cc31217a3e6559afe69163879387a39f8d5e8c
ShahdadKh <mr.khoshbakht91@gmail.com>
2024-07-26 01:12:05 +0330

    session part 2

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import routers from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mochUsers } from "./utils/constants.mjs";
 
 const app = express();
 
@@ -30,4 +31,42 @@ app.get("/", (request, response) => {
   request.session.visited = true;
   response.cookie("Hello", "World", { maxAge: 60000, signed: true });
   response.send({ msg: "Hello" });
+});
+
+app.post("/api/auth", (request, response) => {
+  const {
+    body: { username, password },
+  } = request;
+  const findUser = mochUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return response.status(401).send({ msg: "BAD CREDENTIALS" });
+
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (request, response) => {
+  request.sessionStore.get(request.session.id, (err, sessionData) => {
+    console.log(sessionData);
+  });
+  return request.session.user
+    ? response.status(200).send(request.session.user)
+    : response.status(401).send({ msg: "Not Authenticated" });
+});
+
+app.post("/api/cart", (request, response) => {
+  if (!request.session.user) return response.sendStatus(401);
+  const { body: item } = request;
+  const { cart } = request.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    request.session.cart = [item];
+  }
+  return response.status(201).send(item);
+});
+
+app.get("/api/cart", (request, response) => {
+  if (!request.session.user) return response.sendStatus(401);
+  return response.send(request.session.cart ?? []);
 });
